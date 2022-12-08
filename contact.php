4073ca3736871f1173ed70f19a04045e8bad52bd
moonprincelol <61253224+moonprincelol@users.noreply.github.com>
2022-12-08 16:32:11 +0100

    3

--- contact.php
+++ contact.php
@@ -4,5 +4,12 @@
 Welcome <?php echo $_GET["name"]; ?><br>
 Your email address is: <?php echo $_GET["email"]; ?>
 
+Beste <?php echo $_GET["fname"]; ?><,
+Bedankt voor het invullen van het formulier. We zullen u binnenkort
+via uw <?php echo $_GET["email"]; ?> contacteren .
+Met vriendelijke groet,
+Team
+Uw bericht:
+<?php echo $_GET["message"]; ?>
 </body>
 </html>
