08e50d42a0b05b7edb6c0b162a4c27d8712b8460
DoctorWheel <119934014+DoctorWheel@users.noreply.github.com>
2022-12-09 12:08:22 +0100

    inhoud verkaast

--- contact.php
+++ contact.php
@@ -1,13 +1,13 @@
 <html>
 <body>
-
-
-Beste <?php echo $_GET["firstname"]; ?><,
-Bedankt voor het invullen van het formulier. We zullen u binnenkort
-via uw <?php echo $_GET["email"]; ?> contacteren .
+<pre>
+Beste <?php echo $_GET["firstname"]; ?>,
+Bedankt voor het invullen van het formulier.
+We zullen u binnenkort via <?php echo $_GET["email"]; ?> contacteren.
 Met vriendelijke groet,
 Team
 Uw bericht:
 <?php echo $_GET["message"]; ?>
+</pre>
 </body>
 </html>
