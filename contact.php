5463cf25949885d4c3208b3ee1ea4b4fbc0b83ef
moonprincelol <61253224+moonprincelol@users.noreply.github.com>
2022-12-13 12:20:13 +0100

    php

--- contact.php
+++ contact.php
@@ -5,9 +5,23 @@ Beste <?php echo $_GET["firstname"]; ?>,
 Bedankt voor het invullen van het formulier.
 We zullen u binnenkort via <?php echo $_GET["email"]; ?> contacteren.
 Met vriendelijke groet,
-Team
+Team 
 Uw bericht:
 <?php echo $_GET["message"]; ?>
+<?php
+$to = <?php echo $_GET[email]; ?>;
+$subject = "Bevestinging bericht ontvangen";
+$txt = Beste <?php echo $_GET[firstname]; ?>,
+Bedankt voor het invullen van het formulier.
+We zullen u binnenkort via <?php echo $_GET[email]; ?> contacteren.
+Met vriendelijke groet,
+Team 
+Uw bericht:
+<?php echo $_GET[message]; ?>;
+$headers = "From: IUWGroep10@students.uu.nl" . "\r\n" .;
+
+mail($to,$subject,$txt,$headers);
+?>
 </pre>
 </body>
 </html>
