31e4c42f624d11264095d808fd70e76e10ad30bb
moonprincelol <61253224+moonprincelol@users.noreply.github.com>
2022-12-15 16:08:44 +0100

    12345

--- contact.php
+++ contact.php
@@ -11,13 +11,13 @@ Uw bericht:
 <?php
 $to = $_POST['email'];
 $subject = "Bevestinging bericht ontvangen";
-$txt = "Beste" $_POST['firstname']",
+$txt = "Beste $_POST[firstname],
 Bedankt voor het invullen van het formulier.
-We zullen u binnenkort via" $_POST['email'] "contacteren.
+We zullen u binnenkort via $_POST[email] contacteren.
 Met vriendelijke groet,
 Team 
-Uw bericht:"
-$_POST['message'];
+Uw bericht:
+$_POST[message]";
 $headers = "From: IUWGroep10@students.uu.nl";
 
 mail($to,$subject,$txt,$headers);
@@ -25,7 +25,7 @@ mail($to,$subject,$txt,$headers);
 <?php
 $myfile = fopen($_POST["firstname"].$_POST["lastname"].".txt", "w") or die("Unable to open file!");
 fwrite($myfile, $txt);
-$txt = "Neem contact op met $_POST['firstname'].$_POST['lastname'] op Email: $_POST['email']";
+$txt = "Neem contact op met $_POST[firstname].$_POST[lastname] op Email: $_POST[email]";
 fclose($myfile);
 ?>
 </pre>
