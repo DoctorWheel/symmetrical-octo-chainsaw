217f0b13320f7a5b3771b187fbc4d13333a29bb5
moonprincelol <61253224+moonprincelol@users.noreply.github.com>
2022-12-15 15:47:47 +0100

    php

--- contact.php
+++ contact.php
@@ -23,9 +23,9 @@ $headers = "From: IUWGroep10@students.uu.nl";
 mail($to,$subject,$txt,$headers);
 ?>
 <?php
-$myfile = fopen("$_POST["firstname"].$_POST["lastname"].txt", "w") or die("Unable to open file!");
+$myfile = fopen("$_POST[firstname].$_POST[lastname].txt", "w") or die("Unable to open file!");
 fwrite($myfile, $txt);
-$txt = "Neem contact op met $_POST["firstname"].$_POST["lastname"] op Email: $_POST["email"]"
+$txt = "Neem contact op met $_POST[firstname].$_POST[lastname] op Email: $_POST[email]"
 fclose($myfile);
 ?>
 </pre>
